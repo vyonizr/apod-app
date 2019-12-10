c76ebf3e397abd6ec24bd0b78219420d6867c1b7
Fitrahtur Rahman <fitrahtur@gmail.com>
2019-12-10 14:39:44 +0700

    Added test for accepting a friend request

--- server/src/__test__/users.test.js
+++ server/src/__test__/users.test.js
@@ -1,16 +1,54 @@
 const chai = require('chai')
 const chaiHttp = require('chai-http')
-const expect = chai.expect
+const { expect } = chai
 
 const { UserModel } = require('../models')
 const app = require('../index')
+const { jwt } = require('../helpers')
 
 chai.use(chaiHttp)
+
+const endpoint = {
+  userRegister: '/users/register',
+  userLogin: '/users/login',
+  getAUser: (username) => `/users/profile/${username}`,
+  addAFriend: (username) => `/users/profile/${username}/friends`,
+  acceptFriendRequest: (username, friendUsername) => `/users/profile/${username}/friends/${friendUsername}/accept`,
+  rejectFriendRequest: (username, friendUsername) => `/users/profile/${username}/friends/${friendUsername}/reject`,
+  removeAFriend: (username, friendUsername) => `/users/profile/${username}/friends/${friendUsername}/remove`
+}
+
+const notFoundUsername = 'notfounduser'
+let userToken = null
+let secondUserToken = null
+const firstUser = {
+  username: 'anrupanna',
+  password: 'Apod123'
+}
+
+const secondUser = {
+  username: 'prismassive',
+  password: 'Apod123'
+}
 
 describe('Users test', () => {
   before(async () => {
     try {
       await UserModel.deleteMany({})
+
+      const registeredUser = await UserModel.create(firstUser)
+
+      const secondRegisteredUser = await UserModel.create(secondUser)
+
+      userToken = jwt.sign({
+        id: registeredUser._id,
+        username: registeredUser.username
+      })
+
+      secondUserToken = jwt.sign({
+        id: secondRegisteredUser._id,
+        username: secondRegisteredUser.username
+      })
     } catch (err) {
       console.error(err)
     }
@@ -25,6 +63,8 @@ describe('Users test', () => {
   })
 
   describe('POST /users/register', () => {
+    const { userRegister } = endpoint
+
     describe('ON SUCCESS', () => {
       it('should return status 201 and { object user }', (done) => {
         const objUser = {
@@ -34,7 +74,7 @@ describe('Users test', () => {
 
         chai
         .request(app)
-        .post('/users/register')
+        .post(userRegister)
         .send(objUser)
         .end((err, res) => {
           expect(err).to.equal(null)
@@ -43,8 +83,7 @@ describe('Users test', () => {
           expect(res.body).to.have.property('status')
           expect(res.body).to.have.property('data')
 
-          expect(res.body.status).to.be.a('string')
-          expect(res.body.status).to.have.string('success')
+          expect(res.body.status).to.be.a('string').and.equal('success')
           expect(res.body.data).to.be.an('object')
 
           expect(res.body.data).to.have.property('username')
@@ -65,7 +104,7 @@ describe('Users test', () => {
 
         chai
         .request(app)
-        .post('/users/register')
+        .post(userRegister)
         .send(objUser)
         .end((err, res) => {
           expect(err).to.equal(null)
@@ -74,8 +113,7 @@ describe('Users test', () => {
           expect(res.body).to.have.property('status')
           expect(res.body).to.have.property('message')
 
-          expect(res.body.status).to.be.a('string')
-          expect(res.body.status).to.have.string('fail')
+          expect(res.body.status).to.be.a('string').and.equal('fail')
           expect(res.body.message).to.have.property('errors')
           expect(res.body.message.errors).to.be.an('array')
           done()
@@ -90,7 +128,7 @@ describe('Users test', () => {
 
         chai
         .request(app)
-        .post('/users/register')
+        .post(userRegister)
         .send(objUser)
         .end((err, res) => {
           expect(err).to.equal(null)
@@ -99,8 +137,7 @@ describe('Users test', () => {
           expect(res.body).to.have.property('status')
           expect(res.body).to.have.property('message')
 
-          expect(res.body.status).to.be.a('string')
-          expect(res.body.status).to.have.string('fail')
+          expect(res.body.status).to.be.a('string').and.equal('fail')
           expect(res.body.message).to.have.property('errors')
           expect(res.body.message.errors).to.be.an('array')
           done()
@@ -115,7 +152,7 @@ describe('Users test', () => {
 
         chai
         .request(app)
-        .post('/users/register')
+        .post(userRegister)
         .send(objUser)
         .end((err, res) => {
           expect(err).to.equal(null)
@@ -124,10 +161,8 @@ describe('Users test', () => {
           expect(res.body).to.have.property('status')
           expect(res.body).to.have.property('message')
 
-          expect(res.body.status).to.be.a('string')
-          expect(res.body.status).to.have.string('fail')
-          expect(res.body.message).to.be.a('string')
-          expect(res.body.message).to.have.string('Username is already exist')
+          expect(res.body.status).to.be.a('string').and.equal('fail')
+          expect(res.body.message).to.be.a('string').and.equal('Username is already exist')
           done()
         })
       })
@@ -140,7 +175,7 @@ describe('Users test', () => {
 
         chai
         .request(app)
-        .post('/users/register')
+        .post(userRegister)
         .send(objUser)
         .end((err, res) => {
           expect(err).to.equal(null)
@@ -149,8 +184,7 @@ describe('Users test', () => {
           expect(res.body).to.have.property('status')
           expect(res.body).to.have.property('message')
 
-          expect(res.body.status).to.be.a('string')
-          expect(res.body.status).to.have.string('fail')
+          expect(res.body.status).to.be.a('string').and.equal('fail')
           expect(res.body.message).to.have.property('errors')
           expect(res.body.message.errors).to.be.an('array')
           done()
@@ -160,6 +194,8 @@ describe('Users test', () => {
   })
 
   describe('POST /users/login', () => {
+    const { userLogin } = endpoint
+
     describe('ON SUCCESS', () => {
       it('should return status 200 and { object user }', (done) => {
         const objUser = {
@@ -169,7 +205,7 @@ describe('Users test', () => {
 
         chai
         .request(app)
-        .post('/users/login')
+        .post(userLogin)
         .send(objUser)
         .end((err, res) => {
           expect(err).to.equal(null)
@@ -177,10 +213,9 @@ describe('Users test', () => {
           expect(res.body).to.be.an('object')
           expect(res.body).to.have.property('status')
           expect(res.body).to.have.property('data')
-          expect(res.body.status).to.be.a('string')
+          expect(res.body.status).to.be.a('string').and.equal('success')
           expect(res.body.data).to.be.an('object')
 
-          expect(res.body.status).to.have.string('success')
           expect(res.body.data).to.have.property('username')
           expect(res.body.data).to.have.property('id')
           expect(res.body.data).to.have.property('token')
@@ -201,7 +236,7 @@ describe('Users test', () => {
 
         chai
         .request(app)
-        .post('/users/login')
+        .post(userLogin)
         .send(objUser)
         .end((err, res) => {
           expect(err).to.equal(null)
@@ -223,7 +258,7 @@ describe('Users test', () => {
 
         chai
         .request(app)
-        .post('/users/login')
+        .post(userLogin)
         .send(objUser)
         .end((err, res) => {
           expect(err).to.equal(null)
@@ -231,10 +266,8 @@ describe('Users test', () => {
           expect(res.body).to.be.an('object')
           expect(res.body).to.have.property('status')
           expect(res.body).to.have.property('message')
-          expect(res.body.status).to.be.a('string')
-          expect(res.body.status).to.have.string('fail')
-          expect(res.body.message).to.be.a('string')
-          expect(res.body.message).to.have.string('Wrong username or password')
+          expect(res.body.status).to.be.a('string').and.equal('fail')
+          expect(res.body.message).to.be.a('string').and.equal('Wrong username or password')
           done()
         })
       })
@@ -242,31 +275,30 @@ describe('Users test', () => {
   })
 
   describe('GET /users/profile/:username', () => {
+    const { getAUser } = endpoint
+
     describe('ON SUCCESS', () => {
       it('should return status 200 and { object user }', (done) => {
-        const username = 'annapurna'
-
         chai
         .request(app)
-        .get(`/users/profile/${username}`)
+        .get(getAUser(firstUser.username))
         .end((err, res) => {
           expect(err).to.equal(null)
           expect(res).to.have.status(200)
           expect(res.body).to.be.an('object')
           expect(res.body).to.have.property('status')
           expect(res.body).to.have.property('data')
-          expect(res.body.status).to.be.a('string')
-          expect(res.body.status).to.have.string('success')
+          expect(res.body.status).to.be.a('string').and.equal('success')
           expect(res.body.data).to.be.an('object')
 
+          expect(res.body.data).to.have.property('_id')
           expect(res.body.data).to.have.property('username')
           expect(res.body.data).to.have.property('favorites')
           expect(res.body.data).to.have.property('friends')
           expect(res.body.data).not.to.have.property('password')
           expect(res.body.data).not.to.have.property('role')
 
-          expect(res.body.data.username).to.be.a('string')
-          expect(res.body.data.username).to.have.string(username)
+          expect(res.body.data.username).to.be.a('string').and.equal(firstUser.username)
           expect(res.body.data.favorites).to.be.an('array')
           expect(res.body.data.friends).to.be.an('array')
           done()
@@ -278,7 +310,7 @@ describe('Users test', () => {
       it('should return status 404 and { object error } when username is not found', (done) => {
         chai
         .request(app)
-        .get(`/users/profile/verano`)
+        .get(getAUser(notFoundUsername))
         .end((err, res) => {
           expect(err).to.equal(null)
           expect(res).to.have.status(404)
@@ -286,10 +318,102 @@ describe('Users test', () => {
           expect(res.body).to.have.property('status')
           expect(res.body).to.have.property('message')
 
-          expect(res.body.status).to.be.a('string')
-          expect(res.body.status).to.have.string('fail')
-          expect(res.body.message).to.be.a('string')
-          expect(res.body.message).to.have.string('Username not found')
+          expect(res.body.status).to.be.a('string').and.equal('fail')
+          expect(res.body.message).to.be.a('string').and.equal('Username not found')
+          done()
+        })
+      })
+    })
+  })
+
+  describe('POST /users/profile/:username/friends', () => {
+    const { addAFriend } = endpoint
+    const selfUsername = firstUser.username
+    const targetUsername = secondUser.username
+
+    describe('ON SUCCESS', () => {
+      it('should return status 200 an empty object', (done) => {
+
+        chai
+        .request(app)
+        .post(addAFriend(selfUsername))
+        .set('authentication', userToken)
+        .send({ targetUsername })
+        .end((err, res) => {
+          expect(err).to.equal(null)
+          expect(res).to.have.status(200)
+          expect(res.body).to.be.an('object')
+          expect(res.body).to.have.property('status')
+          expect(res.body).to.have.property('data')
+          expect(res.body.status).to.be.a('string').and.equal('success')
+          expect(res.body.data).to.be.an('object')
+
+          done()
+        })
+      })
+    })
+
+    describe('ON FAIL', () => {
+      it('should return status 400 and { object error } if user adds himself', (done) => {
+        chai
+        .request(app)
+        .post(addAFriend(selfUsername))
+        .set('authentication', userToken)
+        .send({ targetUsername: selfUsername })
+        .end((err, res) => {
+          expect(err).to.equal(null)
+          expect(res).to.have.status(400)
+          expect(res.body).to.be.an('object')
+          expect(res.body).to.have.property('status')
+          expect(res.body).to.have.property('message')
+          expect(res.body.status).to.be.a('string').and.equal('fail')
+          expect(res.body.message).to.be.a('string').and.equal('Cannot send a friend request to oneself')
+
+          done()
+        })
+      })
+
+      it('should return status 404 and { object error } if user adds unregistered username', (done) => {
+        chai
+        .request(app)
+        .post(addAFriend(selfUsername))
+        .set('authentication', userToken)
+        .send({ targetUsername: notFoundUsername })
+        .end((err, res) => {
+          expect(err).to.equal(null)
+          expect(res).to.have.status(404)
+          expect(res.body).to.be.an('object')
+          expect(res.body).to.have.property('status')
+          expect(res.body).to.have.property('message')
+          expect(res.body.status).to.be.a('string').and.equal('fail')
+          expect(res.body.message).to.be.a('string').and.equal('The requested username was not found')
+
+          done()
+        })
+      })
+    })
+  })
+
+  describe('PUT /users/profile/:username/friends/:friendUsername/accept', () => {
+    const { acceptFriendRequest } = endpoint
+    const selfUsername = secondUser.username
+    const targetUsername = firstUser.username
+
+    describe('ON SUCCESS', () => {
+      it('should return status 200 and { object success }', (done) => {
+        chai
+        .request(app)
+        .put(acceptFriendRequest(selfUsername, targetUsername))
+        .set('authentication', secondUserToken)
+        .end((err, res) => {
+          expect(err).to.equal(null)
+          expect(res).to.have.status(200)
+          expect(res.body).to.be.an('object')
+          expect(res.body).to.have.property('status')
+          expect(res.body).to.have.property('data')
+          expect(res.body.status).to.be.a('string').and.equal('success')
+          expect(res.body.data).to.be.an('object')
+
           done()
         })
       })
