da4a21c934eab227d79dd9b06e61dcda8912acef
Fitrahtur Rahman <fitrahtur@gmail.com>
2019-12-20 07:03:24 +0700

    Added /friends route

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const users = require('./users')
 const pictures = require('./pictures')
+const friends = require('./friends')
 
 router.use('/users', users)
 router.use('/pictures', pictures)
+router.use('/friends', friends)
 
 module.exports = router
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,13 +1,8 @@
 const router = require('express').Router()
 const { UserController } = require('../controllers/')
-const { isAuthenticated, isSelfTargeting, isAlreadyAFriend } = require('../middlewares')
 
 router.post('/register', UserController.userRegister)
 router.post('/login', UserController.userLogin)
 router.get('/profile/:username', UserController.getAUser)
-router.post('/profile/:username/friends', isAuthenticated, isSelfTargeting, isAlreadyAFriend, UserController.addAFriend)
-router.put('/profile/:username/friends/:friendUsername/accept', isAuthenticated, UserController.acceptAFriendRequest)
-router.put('/profile/:username/friends/:friendUsername/reject', isAuthenticated, UserController.rejectAFriendRequest)
-router.delete('/profile/:username/friends/:friendUsername/remove', isAuthenticated, UserController.removeAFriend)
 
 module.exports = router
